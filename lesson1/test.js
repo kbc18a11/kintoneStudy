013aeb40c710d13af29a072f7920ae93232ef587
kbc18a11 <wada121400@gmail.com>
2020-12-11 14:46:43 +0900

    今日の授業終わった

--- lesson1/test.js
+++ lesson1/test.js
@@ -1,9 +1,14 @@
 (() => {
   'use strict';
 
+  const todo = [
+    '準備', '実行', '反省'
+  ];
+
   //kintoneのレコード一覧画面が表示された時
   kintone.events.on('app.record.index.show', (event) => {
-    //↓にコンソールへ「テスト」と表示させる処理を追記しましょう
-    console.log('テスト');
+    todo.forEach((todoItem) => {
+      console.log(todoItem);
+    });
   });
 })();
